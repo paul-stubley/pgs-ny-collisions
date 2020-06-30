d03c699811592b2668a4a1d9d2904757951dfa5c
paul-stubley <66024622+paul-stubley@users.noreply.github.com>
2020-06-30 16:38:12 +0100

    [Add] - Add pickled figures file to increase page-load speed
    - Pickled the figures file so app will try to read straight from that rather than doing the analysis every time

--- wrangle_data.py
+++ wrangle_data.py
@@ -2,6 +2,7 @@ import geopandas as gpd
 import pandas as pd
 from shapely.geometry import MultiPolygon
 import plotly.express as px
+import pickle
 
 def return_figures():
     """Creates plotly visualizations
@@ -13,87 +14,99 @@ def return_figures():
         list (dict): list containing the four plotly visualizations
 
     """
+    # Try to load the figures from the saved file
+    try:
+        figures = pickle.load(open("data/figures.pickle", "rb")) 
 
-    # Read in data
-    collisions = gpd.read_file("data/nypd-motor-vehicle-collisions_head.csv")
-    hospitals  = gpd.read_file("data/hospitals/hospitals.shp")
+    # Otherwise create them fresh
+    except:
 
-    # Remove rows with unknown Lat or Long
-    collisions = collisions[collisions.LATITUDE !=""]
-    collisions = collisions[collisions.LONGITUDE!=""]
-    collisions.rename(columns={'LATITUDE':'latitude','LONGITUDE':'longitude'}, inplace=True)
+        # Read in data
+        collisions = gpd.read_file("data/nypd-motor-vehicle-collisions_head.csv")
+        hospitals  = gpd.read_file("data/hospitals/hospitals.shp")
 
-    # Create geometry column
-    collisions.geometry = gpd.points_from_xy(collisions.longitude.astype(float), collisions.latitude.astype(float))
-    collisions.crs = {'init': 'epsg:4326'}    # Plain lat/long is 4326
-    collisions = collisions.to_crs(epsg=2263) # Move to the CRS of the hosital data (geometry column changes, lat/long don't)
+        # Remove rows with unknown Lat or Long
+        collisions = collisions[collisions.LATITUDE !=""]
+        collisions = collisions[collisions.LONGITUDE!=""]
+        collisions.rename(columns={'LATITUDE':'latitude','LONGITUDE':'longitude'}, inplace=True)
 
-    # Fill hospital null capacities with median capacity
-    hospitals = hospitals.fillna(hospitals.capacity.median())
-    hospitals.capacity = hospitals.capacity.astype(float)
-    hospitals.name     = hospitals.name.str.title() # Change case of name column
+        # Create geometry column
+        collisions.geometry = gpd.points_from_xy(collisions.longitude.astype(float), collisions.latitude.astype(float))
+        collisions.crs = {'init': 'epsg:4326'}    # Plain lat/long is 4326
+        collisions = collisions.to_crs(epsg=2263) # Move to the CRS of the hosital data (geometry column changes, lat/long don't)
 
-    # Calculate 10km coverage
-    coverage = pd.DataFrame(hospitals.geometry.buffer(10000), columns = ['buffer'])
-    near_hospitals = hospitals.geometry.buffer(10000).unary_union
-    outside_range = collisions[~collisions.geometry.apply(lambda x: near_hospitals.contains(x))]
+        # Fill hospital null capacities with median capacity
+        hospitals = hospitals.fillna(hospitals.capacity.median())
+        hospitals.capacity = hospitals.capacity.astype(float)
+        hospitals.name     = hospitals.name.str.title() # Change case of name column
 
-    fig_collisions = px.density_mapbox(collisions
-                            , lat='latitude'
-                            , lon='longitude'
-                            , radius=8
-                            , center=dict(lat=40.71, lon=-74)
-                            , zoom=8.9
-                            , mapbox_style="open-street-map"
-                            #, range_color=(0,5) 
-                            , height=600
-                          )
+        # Calculate 10km coverage
+        coverage = pd.DataFrame(hospitals.geometry.buffer(10000), columns = ['buffer'])
+        near_hospitals = hospitals.geometry.buffer(10000).unary_union
+        outside_range = collisions[~collisions.geometry.apply(lambda x: near_hospitals.contains(x))]
 
-    fig_hospitals = px.scatter_mapbox(hospitals
-                            , lat='latitude'
-                            , lon='longitude'
-                            , size='capacity'
-                            , center=dict(lat=40.71, lon=-74)
-                            , zoom=8.9
-                            , text='name'
-                            , color = 'capacity'
-                            , mapbox_style="open-street-map"
-                            , color_continuous_scale='Bluered_r'
-                            , opacity = .9
-                            , height=600
+        fig_collisions = px.density_mapbox(collisions
+                                , lat='latitude'
+                                , lon='longitude'
+                                , radius=8
+                                , center=dict(lat=40.71, lon=-74)
+                                , zoom=8.9
+                                , mapbox_style="open-street-map"
+                                #, range_color=(0,5) 
+                                , height=600
                             )
 
-    fig_hospitals_uncoloured = px.scatter_mapbox(hospitals
-                            , lat='latitude'
-                            , lon='longitude'
-                            , size='capacity'
-                            , text='name'
-                            , center=dict(lat=40.71, lon=-74)
-                            , zoom=8.9
-                            , mapbox_style="open-street-map"
-                            , opacity = .9
-                            , height=600
-                            )
+        fig_hospitals = px.scatter_mapbox(hospitals
+                                , lat='latitude'
+                                , lon='longitude'
+                                , size='capacity'
+                                , center=dict(lat=40.71, lon=-74)
+                                , zoom=8.9
+                                , text='name'
+                                , color = 'capacity'
+                                , mapbox_style="open-street-map"
+                                , color_continuous_scale='Bluered_r'
+                                , opacity = .9
+                                , height=600
+                                )
 
-    fig_uncovered = px.density_mapbox(outside_range
-                            , lat='latitude'
-                            , lon='longitude'
-                            , radius=8
-                            , center=dict(lat=40.71, lon=-74)
-                            , zoom=8.9
-                            , mapbox_style="open-street-map"
-                            #, range_color=(0,5)
-                            , height=600)
+        fig_hospitals_uncoloured = px.scatter_mapbox(hospitals
+                                , lat='latitude'
+                                , lon='longitude'
+                                , size='capacity'
+                                , text='name'
+                                , center=dict(lat=40.71, lon=-74)
+                                , zoom=8.9
+                                , mapbox_style="open-street-map"
+                                , opacity = .9
+                                , height=600
+                                )
 
-    fig_uncovered.add_trace(fig_hospitals_uncoloured.data[0])
+        fig_uncovered = px.density_mapbox(outside_range
+                                , lat='latitude'
+                                , lon='longitude'
+                                , radius=8
+                                , center=dict(lat=40.71, lon=-74)
+                                , zoom=8.9
+                                , mapbox_style="open-street-map"
+                                #, range_color=(0,5)
+                                , height=600)
 
-    layout_fig_collisions = dict(title = 'Vehicle Collisions')
-    layout_fig_hospitals  = dict(title = 'Hospital Capacities')
-    layout_fig_uncovered  = dict(title = 'Collisions more than 10km from the nearest hospital')
+        fig_uncovered.add_trace(fig_hospitals_uncoloured.data[0])
 
-    # append all charts to the figures list
-    figures = []
-    figures.append(dict(data=fig_collisions, layout=layout_fig_collisions))
-    figures.append(dict(data=fig_hospitals, layout=layout_fig_hospitals))
-    figures.append(dict(data=fig_uncovered, layout=layout_fig_uncovered))
+        layout_fig_collisions = dict(title = 'Vehicle Collisions')
+        layout_fig_hospitals  = dict(title = 'Hospital Capacities')
+        layout_fig_uncovered  = dict(title = 'Collisions more than 10km from the nearest hospital')
+
+        # append all charts to the figures list
+        figures = []
+        figures.append(dict(data=fig_collisions, layout=layout_fig_collisions))
+        figures.append(dict(data=fig_hospitals, layout=layout_fig_hospitals))
+        figures.append(dict(data=fig_uncovered, layout=layout_fig_uncovered))
+        
+        # Pickle the figures file for quicker use next time
+        pickle_out = open("data/figures.pickle","wb")
+        pickle.dump(figures, pickle_out)
+        pickle_out.close()
+
     return figures
